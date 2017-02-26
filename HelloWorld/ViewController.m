6e062cef34f5469ebc3de747064454bf1dba5c04
Paul Statezny <paulstatezny@gmail.com>
2017-02-25 22:03:43 -0700

    Build out hello world app with mechanisms from tutorial

--- HelloWorld/ViewController.m
+++ HelloWorld/ViewController.m
@@ -9,14 +9,56 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *simpleLabel;
+@property (weak, nonatomic) IBOutlet UITextField *simpleTextField;
 
 @end
 
 @implementation ViewController
 
+- (IBAction)changeLabel:(id)sender {
+    // Change the label
+    NSString *contents = self.simpleTextField.text;
+    NSString *message = [NSString stringWithFormat:@"Hello, %@", contents];
+    [self.simpleLabel setText:message];
+    
+    [self.simpleTextField resignFirstResponder];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    
+    return YES;
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self.view endEditing:YES];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"viewDidLoad");
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:YES];
+    NSLog(@"viewDidAppear");
+    
+    UIAlertController *alertController = [UIAlertController
+                                          alertControllerWithTitle:@"Some Title"
+                                          message:@"Hey! Wassup"
+                                          preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *defaultAction = [UIAlertAction
+                                    actionWithTitle:@"OK"
+                                    style:UIAlertActionStyleDefault
+                                    handler:^(UIAlertAction * action) {}];
+    
+    defaultAction.enabled = NO;
+    [alertController addAction:defaultAction];
+    
+    [self presentViewController:alertController animated:YES completion:nil];
 }
 
 
